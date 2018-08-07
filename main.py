d103007eec672f70b7ed2a5aeb82d3ac989ecfb5
Ramiro Berrelleza <rberrelleza@gmail.com>
2018-08-07 17:54:09 +0200

    change bgcolor

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-        self.wfile.write("<html><body><h1>hello world!</h1></body></html>")
+        self.wfile.write("<html><body bgcolor= \"#E6E6FA\"><h1>hello world!</h1></body></html>")
 
     def do_HEAD(self):
         self._set_headers()
